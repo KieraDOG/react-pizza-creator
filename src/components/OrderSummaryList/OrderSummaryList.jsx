ff0320eeb7dc7fa70e8ce6e3a3daa72c226fd4fa
Long Zhao <zlong@outlook.com>
2020-03-07 12:29:58 +1100

    Introducing <OrderSummaryList />

---
+++ src/components/OrderSummaryList/OrderSummaryList.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import Section from '../Section';
+import Item from './components/Item';
+
+const OrderSummaryList = () => (
+  <Section
+    title="Order summary"
+  >
+    <Item 
+      pizza={{
+        name: 'Large (13")',
+        price: 9.99,
+      }}
+      toppings={[{
+        name: 'bacon',
+        price: 0.99,
+      }, {
+        name: 'chili',
+        price: 0.99,
+      }]}
+    />
+    <hr />
+  </Section>
+);
+
+export default OrderSummaryList;
